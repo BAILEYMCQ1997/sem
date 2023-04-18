f1130ef5701dc7324bd3c4ddc368af7597bba016
Nathan Murray <40545011@live.napier.ac.uk>
2023-04-18 15:01:24 +0100

    Fifth user case diagram - Nathan
    package com.napier.sem;
    
    /**
     * The type Population.
     */
    public class Population
    {
        /*
        * The name of the continent/region/country.
        * */
        private String locationName;
    
        /*
        * The total population of the continent/region/country.
        * */
        private Long wholeLocationPopulation;
    
        /*
        * The total population of the continent/region/country living in cities (including a %).
        * */

---
+++ src/main/java/com/napier/sem/Population.java
@@ -0,0 +1,19 @@
+package com.napier.sem;
+
+/**
+ * The type Population.
+ */
+public class Population {
+    /*
+     * The name of the continent/region/country.
+     * */
+    private String locationName;
+
+    /*
+     * The total population of the continent/region/country.
+     * */
+    private Long wholeLocationPopulation;
+}
+/*
+ * The total population of the continent/region/country living in cities (including a %).
+ * */
